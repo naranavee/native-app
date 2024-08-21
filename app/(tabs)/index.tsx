22ea2dc95ac550ad238058ab093f8d29f7922e6d
naveen <naranaveen576@gmail.com>
2024-08-21 15:22:40 +0530

    adding input field

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import TextInputExample from '../../components/TextInputExample';
+import TextInputExample from '@/components/TextInputExample';
 
 export default function HomeScreen() {
   return (<TextInputExample/>);
