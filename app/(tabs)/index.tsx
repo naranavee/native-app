da453f871ba4a2ebca4a135f0b205c9f279ac0d8
naveen <naranaveen576@gmail.com>
2024-08-21 15:30:53 +0530

    added Flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import TextInputExample from '@/components/TextInputExample';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import TextInputExample from "@/components/TextInputExample";
+import App from "@/components/FlatList";
 
 export default function HomeScreen() {
-  return (<TextInputExample/>);
+  return <><TextInputExample />
+  <App/></>
+  ;
 }
-
